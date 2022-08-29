c29715533b222adf94ce5ad1deefa86f14a964d5
Banyc <36535895+Banyc@users.noreply.github.com>
2022-08-29 18:38:27 +0800

    feat: build script

---
+++ build_linux.rs
@@ -0,0 +1,18 @@
+#!/usr/bin/env rust-script
+
+use std::{env, process};
+
+fn main() {
+    env::set_var(
+        "CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER",
+        "x86_64-unknown-linux-gnu-gcc"
+    );
+    let output = process::Command::new("cargo")
+        .arg("build")
+        .arg("--release")
+        .arg("--target")
+        .arg("x86_64-unknown-linux-gnu")
+        .output()
+        .unwrap();
+    println!("{:?}", output);
+}
