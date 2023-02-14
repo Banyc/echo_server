71f57afe449eb727171812a5e163a38bb5f00b74
Banyc <36535895+Banyc@users.noreply.github.com>
2023-02-14 13:40:23 +0800

    feat: musl build script

--- build_linux_musl.rs
+++ build_linux_musl.rs
@@ -4,14 +4,14 @@ use std::{env, process};
 
 fn main() {
     env::set_var(
-        "CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER",
-        "x86_64-unknown-linux-gnu-gcc"
+        "CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER",
+        "x86_64-unknown-linux-gnu-gcc",
     );
     let output = process::Command::new("cargo")
         .arg("build")
         .arg("--release")
         .arg("--target")
-        .arg("x86_64-unknown-linux-gnu")
+        .arg("x86_64-unknown-linux-musl")
         .output()
         .unwrap();
     println!("{:?}", output);
